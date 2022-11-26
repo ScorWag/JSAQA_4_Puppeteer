7203c6cd39142bd31c182ddf9d53ca95ed4085ed
ScorWag <scorwag@rambler.ru>
2022-11-26 10:58:36 +0300

    Set timeout for each test

--- gh.test.js
+++ gh.test.js
@@ -11,8 +11,8 @@ afterEach(() => {
 
 describe("Github page tests", () => {
   beforeEach(async () => {
-    await page.goto("https://github.com/team");
-  });
+    await page.goto("https://github.com/team", { timeout: 60000 });
+  }, 60000);
   test("The h1 header content'", async () => {
     const header = "header div div a";
     await page.waitForSelector(header);
@@ -23,14 +23,14 @@ describe("Github page tests", () => {
     expect(title2).toEqual(
       "GitHub for teams · Build like the best teams on the planet · GitHub"
     );
-  });
+  }, 60000);
 
   test("The first link attribute", async () => {
     const link = "a";
     await page.waitForSelector(link);
     const actual = await page.$eval(link, (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 60000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -39,26 +39,26 @@ describe("Github page tests", () => {
     });
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
     expect(actual).toContain("Get started with Team");
-  });
+  }, 60000);
 });
 
 test("The h1 header Features page content'", async () => {
-  await page.goto("https://github.com/features");
+  await page.goto("https://github.com/features", { timeout: 60000 });
   await page.waitForSelector(header);
   const actual = await page.$eval(header, (link) => link.textContent);
   expect(actual).toEqual("The tools you need to build what you want.");
-});
+}, 60000);
 
 test("The h1 header Enterprise page content'", async () => {
-  await page.goto("https://github.com/enterprise");
+  await page.goto("https://github.com/enterprise", { timeout: 60000 });
   await page.waitForSelector(header);
   const actual = await page.$eval(header, (link) => link.textContent);
   expect(actual).toEqual("Build like the best");
-});
+}, 60000);
 
 test("The h1 header Skills page content'", async () => {
-  await page.goto("https://skills.github.com/");
+  await page.goto("https://skills.github.com/", { timeout: 60000 });
   await page.waitForSelector(header);
   const actual = await page.$eval(header, (link) => link.textContent);
   expect(actual).toEqual("GitHub Skills");
-});
+}, 60000);
--- netology.test.js
+++ netology.test.js
@@ -12,25 +12,25 @@ afterEach(() => {
 });
 
 test("The h1 header Netology about page content'", async () => {
-  await page.goto("https://netology.ru/about");
+  await page.goto("https://netology.ru/about", { timeout: 60000 });
   const landingHeader =
     "[class*='src-Landings-pages-About-components-Presentation--title']";
   await page.waitForSelector(landingHeader);
   const actual = await page.$eval(landingHeader, (link) => link.textContent);
   expect(actual).toEqual("Источник знаний для роста в профессии");
-});
+}, 30000);
 
 test("The h1 header Netology softskills page content'", async () => {
-  await page.goto("https://netology.ru/soft-skills");
+  await page.goto("https://netology.ru/soft-skills", { timeout: 60000 });
   await page.waitForSelector(buttonCloseBanner);
   await page.click(buttonCloseBanner);
   const actual = await page.$eval(header, (link) => link.textContent);
   expect(actual).toEqual("Soft Skills");
-});
+}, 30000);
 
 test("The h1 header Netology glossariy page content'", async () => {
-  await page.goto("https://netology.ru/glossariy");
+  await page.goto("https://netology.ru/glossariy", { timeout: 60000 });
   await page.waitForSelector(header);
   const actual = await page.$eval(header, (link) => link.textContent);
   expect(actual).toEqual("Глоссарий");
-});
+}, 30000);
